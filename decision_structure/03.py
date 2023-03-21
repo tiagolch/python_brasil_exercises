815b710f39ac98e56f803fe78d114ee17469f5df
Tiago Chaves <30913654+tiagolch@users.noreply.github.com>
2023-03-21 22:34:24 +0000

    female_or_male
    Signed-off-by: Tiago Chaves <30913654+tiagolch@users.noreply.github.com>

--- decision_structure/03.py
+++ decision_structure/03.py
@@ -1,20 +1,32 @@
 '''
-Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+Faça um Programa que verifique se uma letra digitada é "F" ou "M". 
+Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
 '''
 
 import pytest
 
 
+# letter = input('Digite F ou M: ')
+
+def female_or_male(letter):
+    if letter.upper() == 'F':
+        result = 'Feminino'
+    elif letter.upper() == 'M':
+        result = 'Masculino'
+    else: 
+        result = 'Sexo inválido'
+    print(f'{result}')
+    return result
 
 
-
-
-
+# female_or_male(letter)
 
 
 #tests
-@pytest.mark.parametrize('',[
-    (),
+@pytest.mark.parametrize('letter, expected',[
+    ('f', 'Feminino'),
+    ('m', 'Masculino'),
+    ('h', 'Sexo inválido'),
 ])
-def test_():
-    ...
+def test_female_or_male(letter, expected):
+    assert female_or_male(letter) == expected
