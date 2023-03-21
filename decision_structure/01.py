da9e23b953a240c2758038e1f49ff78ab2140513
Tiago Chaves <30913654+tiagolch@users.noreply.github.com>
2023-03-21 22:08:02 +0000

    largest_number
    Signed-off-by: Tiago Chaves <30913654+tiagolch@users.noreply.github.com>

--- decision_structure/01.py
+++ decision_structure/01.py
@@ -5,17 +5,23 @@ Faça um Programa que peça dois números e imprima o maior deles.
 import pytest
 
 
+# number_a = int(input('Digite o primeiro numero: '))
+# number_b = int(input('Digite o segundo numero: '))
 
 
+def largest_number(number_a, number_b):
+    result = number_a if number_a > number_b else number_b
+    print(f'O maior numero é {result}')
+    return result
 
 
-
-
-
-#tests
-@pytest.mark.parametrize('',[
-    (),
+# tests
+@pytest.mark.parametrize('number_a, number_b, expected', [
+    (1, 2, 2),
+    (10, 2, 10),
+    (-1, 2, 2),
+    (1, -2, 1),
+    (0, 0, 0),
 ])
-def test_():
-    ...
-
+def test_largest_number(number_a, number_b, expected):
+    assert largest_number(number_a, number_b) == expected
