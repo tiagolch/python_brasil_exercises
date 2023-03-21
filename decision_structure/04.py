15a63972f2910a317f2c5917ee88b472bcb7e7b3
Tiago Chaves <30913654+tiagolch@users.noreply.github.com>
2023-03-21 22:53:26 +0000

    vowel_or_consonant
    Signed-off-by: Tiago Chaves <30913654+tiagolch@users.noreply.github.com>

--- decision_structure/04.py
+++ decision_structure/04.py
@@ -5,16 +5,27 @@ Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
 import pytest
 
 
+# letter = input('Digite uma letra: ')
+
+def vowel_or_consonant(letter):
+    if letter.lower() in 'aeiou':
+        result = 'Vogal'
+    elif letter.lower() in 'bcdfghjklmnpqrstvwxyz':
+        result = 'Consoante'
+    else:
+        result = 'Não é uma letra'
+    print(result)
+    return result
 
 
+# vowel_or_consonant(letter)
 
 
-
-
-
-#tests
-@pytest.mark.parametrize('',[
-    (),
+# tests
+@pytest.mark.parametrize('letter, expected', [
+    ('a', 'Vogal'),
+    ('b', 'Consoante'),
+    ('2', 'Não é uma letra'),
 ])
-def test_():
-    ...
+def test_vowel_or_consonant(letter, expected):
+    assert vowel_or_consonant(letter) == expected
