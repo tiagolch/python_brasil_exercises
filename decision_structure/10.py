05c75105f751543190afdb58e696d7f4d2c41567
Tiago Chaves <30913654+tiagolch@users.noreply.github.com>
2023-03-27 12:15:18 +0000

    study_turn
    Signed-off-by: Tiago Chaves <30913654+tiagolch@users.noreply.github.com>

--- decision_structure/10.py
+++ decision_structure/10.py
@@ -6,17 +6,30 @@ Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!
 
 import pytest
 
+# print('Em que turno voce estuda?')
+# turn = input('Digite "M" para matutino, "V" para vespertino ou "N" para noturno: ')
 
+def study_turn(turn):
+    if turn.lower() == 'm':
+        result = 'Bom Dia!'
+    elif turn.lower() == 'v':
+        result = 'Boa Tarde!'
+    elif turn.lower() == 'n':
+        result = 'Boa Noite!'
+    else:
+        result = 'Valor Inválido'
 
+    print(result)
+    return result
 
-
-
-
-
+# study_turn(turn)
 
 #tests
-@pytest.mark.parametrize('',[
-    (),
+@pytest.mark.parametrize('turn, expected',[
+    ('m', 'Bom Dia!'),
+    ('v', 'Boa Tarde!'),
+    ('n', 'Boa Noite!'),
+    ('matutino', 'Valor Inválido'),
 ])
-def test_():
-    ...
+def test_study_turn(turn, expected):
+    assert study_turn(turn) == expected
