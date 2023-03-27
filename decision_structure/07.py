1c2b4ea7bf8e8d32a4478932ae4dfbbe78109bd8
Tiago Chaves <30913654+tiagolch@users.noreply.github.com>
2023-03-27 11:23:02 +0000

    cheaper
    Signed-off-by: Tiago Chaves <30913654+tiagolch@users.noreply.github.com>

--- decision_structure/07.py
+++ decision_structure/07.py
@@ -1,20 +1,35 @@
 '''
-Faça um Programa que leia três números e mostre o maior e o menor deles.
+Faça um Programa que leia três números e mostre o maior e o menor 
+deles.
 '''
 
 import pytest
 
+# value1 = float(input('Valor 1: '))
+# value2 = float(input('Valor 2: '))
+# value3 = float(input('Valor 3: '))
 
 
+def larger_number(value1, value2, value3):
+    if value3 < value2 and value3 < value1:
+        result = value3
+    elif value1 < value2 and value1 < value3:
+        result = value1
+    else:
+        result = value2
+    print(result)
+    return result
 
 
+# larger_number(value1, value2, value3)
 
-
-
-
-#tests
-@pytest.mark.parametrize('',[
-    (),
+# tests
+@pytest.mark.parametrize('value1, value2, value3, expected', [
+    (1, 2, 3, 1),
+    (3, 2, 1, 1),
+    (22, 21, 33, 21),
+    (0, 0, 1, 0),
+    (0, 0, 0, 0),
 ])
-def test_():
-    ...
+def test_larger_number(value1, value2, value3, expected):
+    assert larger_number(value1, value2, value3) == expected
--- decision_structure/08.py
+++ decision_structure/08.py
@@ -1,21 +1,33 @@
 '''
-Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar, 
-sabendo que a decisão é sempre pelo mais barato.
+Faça um programa que pergunte o preço de três produtos e informe qual 
+produto você deve comprar, sabendo que a decisão é sempre pelo mais 
+barato.
 '''
 
 import pytest
 
+# value1 = float(input('Valor do primeiro produto: '))
+# value2 = float(input('Valor do segundo produto: '))
+# value3 = float(input('Valor do terceiro produto: '))
 
 
+def cheaper(value1, value2, value3):
+    if value1 < value2 and value1 < value3:
+        result = value1
+    elif value2 < value1 and value2 < value3:
+        result = value2
+    else:
+        result = value3
+    print(f'{result} é mais barato.')
+    return result
 
+# cheaper(value1, value2, value3)
 
-
-
-
-
-#tests
-@pytest.mark.parametrize('',[
-    (),
+# tests
+@pytest.mark.parametrize('value1, value2, value3, expected', [
+    (10.0, 11.0, 12.0, 10.0),
+    (12.0, 11.0, 10.0, 10.0),
+    (11.0, 10.0, 12.0, 10.0),
 ])
-def test_():
-    ...
+def test_cheaper(value1, value2, value3, expected):
+    assert cheaper(value1, value2, value3) == expected
