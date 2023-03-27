ce09de29e46cc31dfcbc437496226a22788ea722
Tiago Chaves <30913654+tiagolch@users.noreply.github.com>
2023-03-27 11:45:09 +0000

    descending
    Signed-off-by: Tiago Chaves <30913654+tiagolch@users.noreply.github.com>

--- decision_structure/09.py
+++ decision_structure/09.py
@@ -4,17 +4,37 @@ Faça um Programa que leia três números e mostre-os em ordem decrescente.
 
 import pytest
 
+# value1 = float(input('Digite um numero: '))
+# value2 = float(input('Digite um numero: '))
+# value3 = float(input('Digite um numero: '))
 
 
+def descending(value1, value2, value3):
+    if value1 > value2 > value3:
+        result = [value1, value2, value3]
+    elif value1 > value3 > value2:
+        result = [value1, value3, value2]
+    elif value2 > value1 > value3:
+        result = [value2, value1, value3]
+    elif value2 > value3 > value1:
+        result = [value2, value3, value1]
+    elif value3 > value1 > value2:
+        result = [value3, value1, value2]
+    else:
+        result = [value3, value2, value1]
+
+    print(result)
+    return result
+
+# descending(value1, value2, value3)
 
 
-
-
-
-
-#tests
-@pytest.mark.parametrize('',[
-    (),
+# tests
+@pytest.mark.parametrize('value1, value2, value3, expected', [
+    (1, 2, 3, [3, 2, 1]),
+    (2, 1, 3, [3, 2, 1]),
+    (3, 2, 1, [3, 2, 1]),
+    (1, 1, 1, [1, 1, 1])
 ])
-def test_():
-    ...
+def test_descending(value1, value2, value3, expected):
+    assert descending(value1, value2, value3) == expected
