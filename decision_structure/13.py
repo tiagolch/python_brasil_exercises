e70e5679f392ad920ab5200c424028098b532e93
Tiago Chaves <30913654+tiagolch@users.noreply.github.com>
2023-04-03 22:51:01 +0000

    weekday
    Signed-off-by: Tiago Chaves <30913654+tiagolch@users.noreply.github.com>

--- decision_structure/13.py
+++ decision_structure/13.py
@@ -5,17 +5,51 @@ Faça um Programa que leia um número e exiba o dia correspondente da semana.
 
 import pytest
 
+# day = int(input('Digite um dia entre 1 - 31: '))
+
+def weekday(day):
+    domingo = [1, 8, 15, 22, 29]
+    segunda = [2, 9, 16, 23, 30]
+    terca =   [3, 10, 17, 24, 31]
+    quarta =  [4, 11, 18, 25]
+    quinta =  [5, 12, 19, 26]
+    sexta =   [6, 13, 20, 27]
+    sabado =  [7, 14, 21, 28]
+
+    if day in domingo:
+        result = 'Domingo'
+    elif day in segunda:
+        result = 'Segunda'
+    elif day in terca:
+        result = 'Terca'
+    elif day in quarta:
+        result = 'Quarta'
+    elif day in quinta:
+        result = 'Quinta'
+    elif day in sexta:
+        result = 'Sexta'
+    elif day in sabado:
+        result = 'Sabado'
+    else:
+        result = 'Valor inválido'
+
+    print(result)
+    return result
 
 
-
-
-
-
+# weekday(day)
 
 
 #tests
-@pytest.mark.parametrize('',[
-    (),
+@pytest.mark.parametrize('day, expected',[
+    (1, 'Domingo'),
+    (9, 'Segunda'),
+    (24, 'Terca'),
+    (11, 'Quarta'),
+    (26, 'Quinta'),
+    (6, 'Sexta'),
+    (28, 'Sabado'),
+    (32, 'Valor inválido'),
 ])
-def test_():
-    ...
+def test_weekday(day, expected):
+    assert weekday(day) == expected
