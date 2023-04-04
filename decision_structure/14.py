98dbc0aab929cf24810ca136b0d9e6f0b8f15317
Tiago Chaves <30913654+tiagolch@users.noreply.github.com>
2023-04-04 21:42:46 +0000

    grade_point_average
    Signed-off-by: Tiago Chaves <30913654+tiagolch@users.noreply.github.com>

--- decision_structure/14.py
+++ decision_structure/14.py
@@ -1,6 +1,7 @@
 '''
-Faça um programa que lê as duas notas parciais obtidas por um aluno numa disciplina ao longo de um semestre, 
-e calcule a sua média. A atribuição de conceitos obedece à tabela abaixo:
+Faça um programa que lê as duas notas parciais obtidas por um aluno numa disciplina 
+ao longo de um semestre, e calcule a sua média. A atribuição de conceitos obedece à 
+tabela abaixo:
   Média de Aproveitamento  Conceito
   Entre 9.0 e 10.0        A
   Entre 7.5 e 9.0         B
@@ -13,17 +14,73 @@ mensagem “APROVADO” se o conceito for A, B ou C ou “REPROVADO” se o conc
 
 import pytest
 
+# value1 = float(input('Digite a primeira nota: '))
+# value2 = float(input('Digite a segunda nota: '))
 
 
+def grade_point_average(value1, value2):
+    average = (value1 + value2) / 2
+    data = {
+        'value1': value1,
+        'value2': value2,
+        'average': average
+    }
+    
+
+    if average >= 9:
+        concept = 'A'
+        status = 'APROVADO'
+    elif 7.5 <= average < 9:
+        concept = 'B'
+        status = 'APROVADO'
+    elif 6 <= average < 7.5:
+        concept = 'C'
+        status = 'APROVADO'
+    elif 4 <= average < 6:
+        concept = 'D'
+        status = 'REPROVADO'
+    else:
+        concept = 'E'
+        status = 'REPROVADO'
+     
+    data['concept'] = concept
+    data['status'] = status
+
+    print(f'Primeira nota: {value1}')
+    print(f'Segunda nota:  {value2}')
+    print(f'Media:         {data["average"]}')
+    print(f'Conceito:      {data["concept"]}')
+    print(f'Status:        {data["status"]}')
+
+    return data
 
 
-
-
+# grade_point_average(value1, value2)
 
 
 #tests
-@pytest.mark.parametrize('',[
-    (),
+@pytest.mark.parametrize('value1, value2, expected',[
+    (10, 10, {
+        'value1': 10,
+        'value2': 10,
+        'average': 10,
+        'concept': 'A',
+        'status': 'APROVADO'
+    }),
+    (5, 5, {
+        'value1': 5,
+        'value2': 5,
+        'average': 5,
+        'concept': 'D',
+        'status': 'REPROVADO'
+    }),
 ])
-def test_():
-    ...
+def test_grade_point_average(value1, value2, expected):
+    data = grade_point_average(value1, value2)
+
+    assert data['value1'] == expected['value1']
+    assert data['value1'] == expected['value1']
+    assert data['average'] == expected['average']
+    assert data['concept'] == expected['concept']
+    assert data['status'] == expected['status']
+
