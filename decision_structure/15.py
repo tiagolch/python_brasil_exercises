a458f85c9b1fd8917c80576d245daedf67ca948b
Tiago Chaves <30913654+tiagolch@users.noreply.github.com>
2023-04-06 11:40:32 +0000

    triangle
    Signed-off-by: Tiago Chaves <30913654+tiagolch@users.noreply.github.com>

--- decision_structure/15.py
+++ decision_structure/15.py
@@ -1,5 +1,6 @@
 '''
-Faça um Programa que peça os 3 lados de um triângulo. O programa deverá informar se os valores podem ser um triângulo. 
+Faça um Programa que peça os 3 lados de um triângulo. O programa deverá informar se os 
+valores podem ser um triângulo. 
 Indique, caso os lados formem um triângulo, se o mesmo é: equilátero, isósceles ou escaleno.
 Dicas:
 Três lados formam um triângulo quando a soma de quaisquer dois lados for maior que o terceiro;
@@ -10,17 +11,29 @@ Triângulo Escaleno: três lados diferentes;
 
 import pytest
 
+# a = float(input('valor do lado A: '))
+# b = float(input('valor do lado B: '))
+# c = float(input('valor do lado C: '))
 
 
+def triangle(a, b, c):
+    if a == b == c:
+        result = 'Equilátero'
+    elif (a == b) or (b == c) or (c == a):
+        result = 'Isósceles'
+    else:
+        result = 'Escaleno'
+    return result
 
 
+# triangle(a, b, c)
 
 
-
-
-#tests
-@pytest.mark.parametrize('',[
-    (),
+# tests
+@pytest.mark.parametrize('a, b, c, expected', [
+    (2, 2, 2, 'Equilátero'),
+    (2, 2, 3, 'Isósceles'),
+    (1, 2, 3, 'Escaleno')
 ])
-def test_():
-    ...
+def test_triangle(a, b, c, expected):
+    assert triangle(a, b, c) == expected
