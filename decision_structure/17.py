7e2fec171363de2c7e5d65c6f8b740540c1ab77a
Tiago Chaves <30913654+tiagolch@users.noreply.github.com>
2023-04-09 23:02:20 +0000

    bisext_year
    Signed-off-by: Tiago Chaves <30913654+tiagolch@users.noreply.github.com>

--- decision_structure/17.py
+++ decision_structure/17.py
@@ -1,20 +1,31 @@
 '''
-Faça um Programa que peça um número correspondente a um determinado ano e em seguida informe se este ano é ou não bissexto.
+Faça um Programa que peça um número correspondente a um determinado ano e em 
+seguida informe se este ano é ou não bissexto.
 '''
 
 import pytest
 
 
+# year = int(input('Digite um ano para saber se é bisexto: '))
+
+def bisext_year(year):
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+        result = True
+    else:
+        result = False
+    
+    print(result)
+    return result
 
 
-
-
-
+# bisext_year(year)
 
 
 #tests
-@pytest.mark.parametrize('',[
-    (),
+@pytest.mark.parametrize('year, expected',[
+    (2000, True),
+    (2016, True),
+    (2023, False),
 ])
-def test_():
-    ...
+def test_bisext_year(year, expected):
+    assert bisext_year(year) == expected
