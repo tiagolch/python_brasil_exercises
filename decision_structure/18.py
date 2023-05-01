f65acbd5af551c8f30abaa9f94b1be8ffb4701fb
Tiago Chaves <30913654+tiagolch@users.noreply.github.com>
2023-05-01 20:54:47 +0000

    validate_date
    Signed-off-by: Tiago Chaves <30913654+tiagolch@users.noreply.github.com>

--- decision_structure/18.py
+++ decision_structure/18.py
@@ -2,19 +2,25 @@
 Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesma é uma data válida.
 '''
 
+from datetime import datetime
 import pytest
 
+#date = input('Insira uma data no formato dd/mm/YYYY: ')
+
+def validate_date(date):
+    if datetime.strptime(date, "%d/%m/%Y"):
+        result = True
+    else:
+        result = False
+    return result
 
 
-
-
-
-
+#validate_date(date)
 
 
 #tests
-@pytest.mark.parametrize('',[
-    (),
+@pytest.mark.parametrize('date, expected',[
+    ('11/11/2023', True),
 ])
-def test_():
-    ...
+def test_validate_date(date, expected):
+    assert validate_date(date) == expected
